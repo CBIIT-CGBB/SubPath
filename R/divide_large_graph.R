02a5be2f1e98ddb726e44ceac3b5f6833140d814
Ying Hu <yhu@mail.nih.gov>
2024-04-12 15:54:31 -0400

    Update divide_large_graph.R

--- R/divide_large_graph.R
+++ R/divide_large_graph.R
@@ -8,17 +8,21 @@ divide_large_graph <- function(graph, max_nodes=10) {
   small_subgraphs <- list()
   
   # Iterate through each subgraph in the clusters
-  for (i in 1:length(clusters)) {
-    subgraph <- induced_subgraph(graph, clusters[[i]])
+  if (length(clusters)==1){ ## the graph cannot be divided by cluster_louvain
+    subgraph <- induced_subgraph(graph, clusters[[1]])
+    small_subgraphs[[length(small_subgraphs) + 1]] <- subgraph
+  } else {
+    for (i in 1:length(clusters)) {    
+      subgraph <- induced_subgraph(graph, clusters[[i]])
     
-    # Check if the subgraph has fewer or equal to max_nodes
-    if (vcount(subgraph) <= max_nodes) {
-      small_subgraphs[[length(small_subgraphs) + 1]] <- subgraph
-    } else {
-      # If the subgraph is still too large, recursively divide it further
-      small_subgraphs <- c(small_subgraphs, divide_large_graph(subgraph, max_nodes))
+      # Check if the subgraph has fewer or equal to max_nodes
+      if (vcount(subgraph) <= max_nodes) {
+        small_subgraphs[[length(small_subgraphs) + 1]] <- subgraph
+      } else {
+        # If the subgraph is still too large, recursively divide it further
+        small_subgraphs <- c(small_subgraphs, divide_large_graph(subgraph, max_nodes))
+      }
     }
   }
-  
   return(small_subgraphs)
 }
